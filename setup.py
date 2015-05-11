d6f425e43c170931bc88720e40119b2acedacd0e
Nozomu Kaneko <nozom.kaneko@gmail.com>
2015-05-11 15:52:19 +0900

    use swig if available

--- setup.py
+++ setup.py
@@ -5,10 +5,30 @@ try:
 except ImportError:
     from distutils.core import setup, Extension
 
-ext_module = Extension('_Mykytea',
-                       sources=['mykytea_wrap.cxx', 'mykytea.cpp'],
-                       libraries=["kytea"],
-                       )
+def has_swig():
+    import os
+    import subprocess
+
+    devnull = open(os.devnull, 'w')
+    retcode = subprocess.call(['swig', '-version'], stdout=devnull)
+    return retcode == 0
+
+use_swig = has_swig()
+
+if use_swig:
+    ext_module = Extension(
+        '_Mykytea',
+        sources=['mykytea.i', 'mykytea.cpp'],
+        libraries=["kytea"],
+        swig_opts=['-Wall', '-shadow', '-c++', '-I/usr/local/include'],
+        )
+
+else:
+    ext_module = Extension(
+        '_Mykytea',
+        sources=['mykytea_wrap.cxx', 'mykytea.cpp'],
+        libraries=["kytea"],
+        )
 
 setup(name='kytea-python',
       version='0.1',
